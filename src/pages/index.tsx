5297444af069bebe6be3561a72d5695756cf9b10
MatheusF99 <mc79846@gmail.com>
2021-07-23 12:27:21 -0300

    configurando component header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='asd'>
-      <Head>
-        <title>Podcastr</title>
-      </Head>
-
       <h1>hello world</h1>
     </div>
   )
