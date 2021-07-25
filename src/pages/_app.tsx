a18985d0f2bb5235e06b691498df900d6755e5ec
MatheusF99 <mc79846@gmail.com>
2021-07-25 20:27:05 -0300

    criando player terminado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import Header from '../components/Header/Header'
+import Player from '../components/Player/Player'
+
 import '../styles/globals.scss'
+import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
     </div>
   )
 }
