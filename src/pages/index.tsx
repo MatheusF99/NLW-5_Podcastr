0bcbeaab3fb28cec3e212a9572b46f0a3d334bcc
MatheusF99 <mc79846@gmail.com>
2021-07-31 18:56:35 -0300

    criando componente home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,80 @@
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
 
-export default function Home() {
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+
+import styles from '../styles/home.module.scss'
+
+interface EpisodesProps {
+  id: string;
+  title: string;
+  members: string,
+  published_at: string,
+  thumbnail: string,
+  description: string,
+  duration: Number,
+  durationAsString: string
+  url: string
+
+}
+
+interface homeProps {
+  episodes: EpisodesProps[]
+}
+
+export default function Home(props: homeProps) {
   return (
-    <div className='asd'>
-      <h1>hello world</h1>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Ultimos Lançamentos</h2>
+        <ul>
+
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+
+      </section>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const data = response.data
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MM yy', {
+        locale: ptBR
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+      description: episode.description
+    }
+  })
+
+  const lastestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.length)
+
+  return {
+    props: {
+      lastestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
